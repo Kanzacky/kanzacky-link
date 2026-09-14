46292d9aad35838348dad0581b3216f80d7f1812
Kanzacky <zadie294@gmail.com>
2026-09-14 18:24:45 +0700

    feat: samakan ukuran logo unipma dengan nekat 84px

--- components/About.tsx
+++ components/About.tsx
@@ -119,7 +119,7 @@ export default function About() {
                   <p className="text-[12px] text-theme-muted transition-colors duration-500">Teknik Informatika</p>
                 </div>
                 <div className="shrink-0 ml-3 mr-2">
-                  <img src="/unipma.png" alt="UNIPMA Logo" className="w-[50px] h-[50px] object-contain opacity-80 group-hover:opacity-100 transition-opacity duration-500" />
+                  <img src="/unipma.png" alt="UNIPMA Logo" className="w-[84px] h-[84px] object-contain opacity-80 group-hover:opacity-100 transition-opacity duration-500" />
                 </div>
               </div>
             </motion.div>
