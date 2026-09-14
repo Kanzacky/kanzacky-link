d2b1252df659a59eb925aafcbaeb1a7b1b3af580
Kanzacky <zadie294@gmail.com>
2026-09-14 18:36:36 +0700

    feat: geser logo nekatv2 sedikit ke kiri

--- components/About.tsx
+++ components/About.tsx
@@ -85,7 +85,7 @@ export default function About() {
                     Teknik Kendaraan Ringan
                   </p>
                 </div>
-                <div className="shrink-0 ml-3 mr-1">
+                <div className="shrink-0 ml-1 mr-1">
                   <img src="/nekatv2.png" alt="SMK Negeri Kartoharjo Logo" className="w-[60px] h-[60px] sm:w-[84px] sm:h-[84px] object-contain opacity-80 group-hover:opacity-100 transition-opacity duration-500" />
                 </div>
               </div>
